2d434008d6d0a9e7dda56abd4603d3682925ca5e
malienist <safalpillai@gmail.com>
2020-05-23 15:33:55 +0530

    addded eslint config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RootComponent } from '@components/root/root.component';
 import { SharedModule } from '@shared/shared.module';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
 
 @NgModule({
     declarations: [
         AppComponent,
-        RootComponent
+        RootComponent,
     ],
     imports: [
         BrowserModule,
@@ -21,6 +21,6 @@ import { SharedModule } from '@shared/shared.module';
         SharedModule,
     ],
     providers: [],
-    bootstrap: [AppComponent]
+    bootstrap: [AppComponent],
 })
 export class AppModule { }
