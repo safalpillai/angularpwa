4026bfdf176bc66bb2997b34f6ce91ed1c11ff21
malienist <safalpillai@gmail.com>
2020-05-23 23:07:41 +0530

    install app prompt issue

--- src/app/components/root/root.component.ts
+++ src/app/components/root/root.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { ApiService, IItem } from '@services/api.service';
 
 @Component({
@@ -8,6 +8,14 @@ import { ApiService, IItem } from '@services/api.service';
 })
 export class RootComponent implements OnInit {
     items: IItem[];
+    deferredPrompt: any;
+    
+    @HostListener('window:beforeinstallprompt', ['$event']) 
+    beforeinstallprompt(event) {
+        console.log('HOSTLISTENER', event);
+        event.preventDefault();
+        this.deferredPrompt = event;
+    }
 
     constructor(
         private apiService: ApiService,
@@ -17,10 +25,41 @@ export class RootComponent implements OnInit {
         this.fetchData();
     }
 
+    addToHomeScreen() {
+        this.deferredPrompt.prompt();
+        this.deferredPrompt.userChoice.then(choice => {
+            if (choice.outcome === 'accepted') console.log('USER ACCEPTED A2H');
+            else console.log('USER REJECTED A2H');
+            this.deferredPrompt = null;
+        });
+    }
+
     fetchData() {
         this.apiService.fetch().subscribe((data: IItem[]) => {
-            console.log('DATA', data);
-            this.items = data;
+            this.items = this.randomizeData(data);
         });
+    }
+
+    randomizeData(data: any[]): any[] {
+        const pushedIndexes: number[] = [];
+        const randomData: any[] = [];
+        while (data?.length !== randomData?.length) {
+            let random;
+            if (data?.length === randomData?.length) break;
+            else { 
+                random = this.nonRepeatingRandomNumber(data.length);
+                if (!pushedIndexes.includes(random)) {
+                    randomData.push(data[random]);
+                    pushedIndexes.push(random);
+                }
+            }
+        }
+        return randomData;
+    }
+
+    random: number = -1;
+    nonRepeatingRandomNumber(end: number): number {
+        const _random = Math.ceil(Math.random() * end - 1);
+        return _random !== this.random ? (this.random = _random, _random) : this.nonRepeatingRandomNumber(end);
     }
 }
